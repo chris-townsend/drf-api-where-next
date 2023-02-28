10a400d39d85245a4c2e55ebc30cee887cff45b9
chris-townsend <chris-townsend@live.co.uk>
2023-02-28 15:28:48 +0000

    Close #1 Create profiles app with custom Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,201 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    User Profile Model with COUNTRIES list for location
+    """
+
+    COUNTRIES = [
+        ('Afghanistan', 'Afghanistan'),
+        ('Albania', 'Albania'),
+        ('Algeria', 'Algeria'),
+        ('Andorra', 'Andorra'),
+        ('Angola', 'Angola'),
+        ('Antigua', 'Antigua'),
+        ('Argentina', 'Argentina'),
+        ('Armenia', 'Armenia'),
+        ('Australia', 'Australia'),
+        ('Austria', 'Austria'),
+        ('Azerbaijan', 'Azerbaijan'),
+        ('The Bahamas', 'The Bahamas'),
+        ('Bahrain', 'Bahrain'),
+        ('Bangladesh', 'Bangladesh'),
+        ('Barbados', 'Barbados'),
+        ('Belarus', 'Belarus'),
+        ('Belgium', 'Belgium'),
+        ('Belize', 'Belize'),
+        ('Bhutan', 'Bhutan'),
+        ('Bolivia', 'Bolivia'),
+        ('Bosnia', 'Bosnia'),
+        ('Botswana', 'Botswana'),
+        ('Brazil', 'Brazil'),
+        ('Brunei', 'Brunei'),
+        ('Bulgaria', 'Bulgaria'),
+        ('Cambodia', 'Cambodia'),
+        ('Cameroon', 'Cameroon'),
+        ('Canada', 'Canada'),
+        ('Central African Republic', 'Central African Republic'),
+        ('Chile', 'Chile'),
+        ('China', 'China'),
+        ('Colombia', 'Colombia'),
+        ('Congo, Republic of the', 'Congo, Republic of the'),
+        ('Costa Rica', 'Costa Rica'),
+        ('Croatia', 'Croatia'),
+        ('Cuba', 'Cuba'),
+        ('Cyprus', 'Cyprus'),
+        ('Czech Republic', 'Czech Republic'),
+        ('Denmark', 'Denmark'),
+        ('Dominica', 'Dominica'),
+        ('Dominican Republic', 'Dominican Republic'),
+        ('Ecuador', 'Ecuador'),
+        ('Egypt', 'Egypt'),
+        ('El Salvador', 'El Salvador'),
+        ('Equatorial Guinea', 'Equatorial Guinea'),
+        ('Estonia', 'Estonia'),
+        ('Eswatini', 'Eswatini'),
+        ('Ethiopia', 'Ethiopia'),
+        ('Fiji', 'Fiji'),
+        ('Finland', 'Finland'),
+        ('France', 'France'),
+        ('Gabon', 'Gabon'),
+        ('Georgia', 'Georgia'),
+        ('Germany', 'Germany'),
+        ('Ghana', 'Ghana'),
+        ('Greece', 'Greece'),
+        ('Grenada', 'Grenada'),
+        ('Guatemala', 'Guatemala'),
+        ('Guinea', 'Guinea'),
+        ('Guyana', 'Guyana'),
+        ('Haiti', 'Haiti'),
+        ('Honduras', 'Honduras'),
+        ('Hungary', 'Hungary'),
+        ('Iceland', 'Iceland'),
+        ('India', 'India'),
+        ('Indonesia', 'Indonesia'),
+        ('Iran', 'Iran'),
+        ('Iraq', 'Iraq'),
+        ('Ireland', 'Ireland'),
+        ('Israel', 'Israel'),
+        ('Italy', 'Italy'),
+        ('Jamaica', 'Jamaica'),
+        ('Japan', 'Japan'),
+        ('Jordan', 'Jordan'),
+        ('Kazakhstan', 'Kazakhstan'),
+        ('Kenya', 'Kenya'),
+        ('Korea, North', 'Korea, North'),
+        ('Korea, South', 'Korea, South'),
+        ('Kosovo', 'Kosovo'),
+        ('Kuwait', 'Kuwait'),
+        ('Kyrgyzstan', 'Kyrgyzstan'),
+        ('Laos', 'Laos'),
+        ('Latvia', 'Latvia'),
+        ('Lebanon', 'Lebanon'),
+        ('Lesotho', 'Lesotho'),
+        ('Liberia', 'Liberia'),
+        ('Libya', 'Libya'),
+        ('Lithuania', 'Lithuania'),
+        ('Luxembourg', 'Luxembourg'),
+        ('Madagascar', 'Madagascar'),
+        ('Malawi', 'Malawi'),
+        ('Malaysia', 'Malaysia'),
+        ('Maldives', 'Maldives'),
+        ('Mali', 'Mali'),
+        ('Malta', 'Malta'),
+        ('Mexico', 'Mexico'),
+        ('Moldova', 'Moldova'),
+        ('Monaco', 'Monaco'),
+        ('Mongolia', 'Mongolia'),
+        ('Montenegro', 'Mongolia'),
+        ('Morocco', 'Morocco'),
+        ('Mozambique', 'Mozambique'),
+        ('Myanmar', 'Myanmar'),
+        ('Nepal', 'Nepal'),
+        ('Netherlands', 'Netherlands'),
+        ('New Zealand', 'New Zealand'),
+        ('Nicaragua', 'Nicaragua'),
+        ('Nigeria', 'Nigeria'),
+        ('North Macedonia', 'North Macedonia'),
+        ('Norway', 'Norway'),
+        ('Oman', 'Oman'),
+        ('Pakistan', 'Pakistan'),
+        ('Panama', 'Panama'),
+        ('Papua New Guinea', 'Papua New Guinea'),
+        ('Paraguay', 'Paraguay'),
+        ('Peru', 'Peru'),
+        ('Philippines', 'Philippines'),
+        ('Poland', 'Poland'),
+        ('Portugal', 'Portugal'),
+        ('Qatar', 'Qatar'),
+        ('Romania', 'Romania'),
+        ('Russia', 'Russia'),
+        ('Rwanda', 'Rwanda'),
+        ('San Marino', 'San Marino'),
+        ('Saudi Arabia', 'Saudi Arabia'),
+        ('Senegal', 'Senegal'),
+        ('Serbia', 'Serbia'),
+        ('Seychelles', 'Seychelles'),
+        ('Sierra Leone', 'Sierra Leone'),
+        ('Singapore', 'Singapore'),
+        ('Slovakia', 'Slovakia'),
+        ('Slovenia', 'Slovenia'),
+        ('Somalia', 'Somalia'),
+        ('South Africa', 'South Africa'),
+        ('Spain', 'Spain'),
+        ('Sri Lanka', 'Sri Lanka'),
+        ('Sudan', 'Sudan'),
+        ('Sweden', 'Sweden'),
+        ('Switzerland', 'Switzerland'),
+        ('Syria', 'Syria'),
+        ('Taiwan', 'Taiwan'),
+        ('Tajikistan', 'Tajikistan'),
+        ('Tanzania', 'Tanzania'),
+        ('Thailand', 'Thailand'),
+        ('Tonga', 'Tonga'),
+        ('Trinidad', 'Trinidad'),
+        ('Tunisia', 'Tunisia'),
+        ('Turkey', 'Turkey'),
+        ('Turkmenistan', 'Turkmenistan'),
+        ('Uganda', 'Uganda'),
+        ('Ukraine', 'Ukraine'),
+        ('United Arab Emirates', 'United Arab Emirates'),
+        ('United Kingdom', 'United Kingdom'),
+        ('United States', 'United States'),
+        ('Uruguay', 'Uruguay'),
+        ('Uzbekistan', 'Uzbekistan'),
+        ('Vatican City', 'Vatican City'),
+        ('Venezuela', 'Venezuela'),
+        ('Vietnam', 'Vietnam'),
+        ('Yemen', 'Yemen'),
+        ('Zambia', 'Zambia'),
+        ('Zimbabwe', 'Zimbabwe')
+    ]
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100, blank=True)
+    location = models.CharField(max_length=50, choices=COUNTRIES, blank=True)
+    favourite_location = models.CharField(max_length=50, blank=True)
+    bio = models.TextField(blank=True)
+    profile_image = models.ImageField(
+        upload_to='images/', default='../default_user_s9bxgj')
+
+    class Meta:
+        """
+        Return profile instances in reverse order so newest is first
+        """
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        """
+        Creates a profile whenever a new user is created
+        """
+        if created:
+            Profile.objects.create(owner=instance)
