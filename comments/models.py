67448a9dc66885b818f48dd5b4c0c5606be83d08
chris-townsend <chris-townsend@live.co.uk>
2023-03-02 11:55:04 +0000

    Close #9 Add likes app with Like model

--- comments/models.py
+++ comments/models.py
@@ -4,9 +4,9 @@ from posts.models import Post
 
 
 class Comment(models.Model):
-    '''
+    """
     Comment model, which is related to User and Post
-    '''
+    """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
