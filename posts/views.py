6459981a5fcafc5f9efc3d8bc40012a9d3920e49
chris-townsend <chris-townsend@live.co.uk>
2023-03-02 08:08:23 +0000

    Add PostDetail view and wire up URL

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
 from .serializers import PostSerializer
+from where_next_drf_api.permissons import IsOwnerOrReadOnly
 
 
 class PostList(APIView):
@@ -30,3 +32,23 @@ class PostList(APIView):
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
         )
+
+
+class PostDetail(APIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    """
+    Display a post detail page
+    """
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, context={'request': request})
+        return Response(serializer.data)
