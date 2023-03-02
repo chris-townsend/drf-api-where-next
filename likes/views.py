23c9c4b9e369a6c661f0192832cea21fac52bb40
chris-townsend <chris-townsend@live.co.uk>
2023-03-02 12:51:22 +0000

    Add LikeDetail view and wire up urlpattern

--- likes/views.py
+++ likes/views.py
@@ -14,3 +14,12 @@ class LikeList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like and delete it by id if the user is the owner & logged-in
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
