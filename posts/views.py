d3cc82777d9b34b3a8d9a5c5401f9c62d3244fee
chris-townsend <chris-townsend@live.co.uk>
2023-03-02 08:24:36 +0000

    Close #5 Add delete functionality to the PostDetail class

--- posts/views.py
+++ posts/views.py
@@ -8,6 +8,9 @@ from where_next_drf_api.permissons import IsOwnerOrReadOnly
 
 
 class PostList(APIView):
+    """
+    Display a list of posts
+    """
     serializer_class = PostSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
@@ -52,11 +55,23 @@ class PostDetail(APIView):
         post = self.get_object(pk)
         serializer = PostSerializer(post, context={'request': request})
         return Response(serializer.data)
-    
+
     def put(self, request, pk):
+        """
+        Allows the owner of the post to update/edit the post
+        """
         post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data, context={'request': request})
+        serializer = PostSerializer(
+            post, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        """
+        Allows the owner of the post to delete the post
+        """
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
