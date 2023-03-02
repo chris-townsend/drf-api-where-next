63cb515d0ae195314937c891c4dbb05a822a010e
chris-townsend <chris-townsend@live.co.uk>
2023-03-02 11:15:06 +0000

    Create CommentList ListCreate view & import naturaltime in comments serializers.py

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from where_next_drf_api.permissons import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    """
+    List comments or create a comment if a user is logged-in
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
