0a97f29fd754ecc0fa68075b19fa4d8c3f9924d4
chris-townsend <chris-townsend@live.co.uk>
2023-03-03 12:17:11 +0000

    Add followers/following count to queryset of ProfileList & order by created_date

--- profiles/views.py
+++ profiles/views.py
@@ -12,7 +12,9 @@ class ProfileList(generics.ListAPIView):
     """
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
-    )
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_date')
     serializer_class = ProfileSerializer
 
 
