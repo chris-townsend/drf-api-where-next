baea319176ee2432612df1fbac543b1f0257de46
chris-townsend <chris-townsend@live.co.uk>
2023-03-03 13:49:18 +0000

    Add filter to comments app and specify by post in comments views.py

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, permissions
 from where_next_drf_api.permissons import IsOwnerOrReadOnly
 from .models import Comment
@@ -11,6 +12,9 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+
+    filterset_fields = ['post']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
