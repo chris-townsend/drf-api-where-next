77437911ed7bcd50254276d65fffdcaee3c3d3ac
chris-townsend <chris-townsend@live.co.uk>
2023-03-03 08:55:01 +0000

    Add date of birth field to Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -177,6 +177,7 @@ class Profile(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=100, blank=True)
+    date_of_birth = models.DateField(blank=True, null=True)
     location = models.CharField(max_length=50, choices=COUNTRIES, blank=True)
     favourite_location = models.CharField(max_length=50, blank=True)
     bio = models.TextField(blank=True)
