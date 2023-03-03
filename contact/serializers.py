867f9937f195a171c0499ff10e66834fd0c2c49d
chris-townsend <chris-townsend@live.co.uk>
2023-03-03 10:10:58 +0000

    Close #16 Create ContactSerializer in serializers.py of contact app

---
+++ contact/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import ContactForm
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the ContactForm model
+    """
+    owner = serializers.ReadOnlyField(source="owner.username")
+    profile_id = serializers.ReadOnlyField(source="owner.profile.id")
+    profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    created_date = serializers.SerializerMethodField()
+    updated_date = serializers.SerializerMethodField()
+
+    class Meta:
+        """
+        Specify fields from ContactForm model
+        """
+
+        model = ContactForm
+        fields = [
+            "id",
+            "owner",
+            "subject",
+            "message",
+            "profile_id",
+            "profile_image",
+            "created_date",
+            "updated_date",
+        ]
