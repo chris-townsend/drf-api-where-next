07d97b8acac56b42ceb049808e60523aad3befed
chris-townsend <chris-townsend@live.co.uk>
2023-03-03 11:12:37 +0000

    Add naturaltime to display created/updated date in posts app

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Post
 
@@ -12,6 +13,20 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_date = serializers.SerializerMethodField()
+    updated_date = serializers.SerializerMethodField()
+
+    def get_created_date(self, obj):
+        """
+        Returns a human readable time for created profile date
+        """
+        return naturaltime(obj.created_date)
+
+    def get_updated_date(self, obj):
+        """
+        Returns a human readable time for updated profile date
+        """
+        return naturaltime(obj.updated_date)
 
     def validate_image(self, value):
         """
