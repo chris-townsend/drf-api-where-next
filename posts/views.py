18b6d52732fab387adc01dbf6767d41a1fdfb56e
chris-townsend <chris-townsend@live.co.uk>
2023-03-03 12:45:32 +0000

    Add queryset items to PostList & PostDetail classes in posts views.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
@@ -12,7 +13,10 @@ class PostList(generics.ListCreateAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count('comment', distinct=True),
+        likes_count=Count('likes', distinct=True)
+    ).order_by('-created_date')
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -25,4 +29,7 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count('comment', distinct=True),
+        likes_count=Count('likes', distinct=True)
+    ).order_by('-created_date')
