5aa1381a7a8e15f09f49d68b04b95404d3193c88
chris-townsend <chris-townsend@live.co.uk>
2023-03-03 11:06:16 +0000

    Add naturaltime to display created/updated date in profiles app

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Profile
 
@@ -10,10 +11,24 @@ class ProfileSerializer(serializers.ModelSerializer):
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    created_date = serializers.SerializerMethodField()
+    updated_date = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_created_date(self, obj):
+        """
+        Returns a human readable time for created profile date
+        """
+        return naturaltime(obj.created_date)
+
+    def get_updated_date(self, obj):
+        """
+        Returns a human readable time for updated profile date
+        """
+        return naturaltime(obj.updated_date)
 
     class Meta:
         """
