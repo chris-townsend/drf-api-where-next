9933008e36fb96c42ecc95902a974f51c99aa755
chris-townsend <chris-townsend@live.co.uk>
2023-03-03 13:29:23 +0000

    Add filter feature to posts & specify filterset_fields in views.py

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, permissions, filters
 from .models import Post
 from .serializers import PostSerializer
@@ -21,18 +22,26 @@ class PostList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
-        ]
+        DjangoFilterBackend,
+    ]
 
     search_fields = [
         'title',
         'owner__username',
-        ]
+    ]
 
     ordering_fields = [
         'likes_count',
         'comments_count',
         'likes__created_date',
-        ]
+    ]
+
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'bookmark__owner__profile',
+        'owner__profile',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
