04bfdce0b227db2d78feee80d5d5e56a9a531b2f
chris-townsend <chris-townsend@live.co.uk>
2023-03-04 10:30:32 +0000

    Add DATETIME_FORMAT to REST_FRAMEWORK default pagination class in settings.py

--- where_next_drf_api/settings.py
+++ where_next_drf_api/settings.py
@@ -38,6 +38,7 @@ REST_FRAMEWORK = {
         'DEFAULT_PAGINATION_CLASS':
             'rest_framework.pagination.PageNumberPagination',
             'PAGE_SIZE': 10,
+            'DATETIME_FORMAT': '%d %b %Y',
     }
 
 if 'DEV' not in os.environ:
