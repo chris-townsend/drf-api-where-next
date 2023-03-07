2fb1f29d687642d668207b2b20e74727f869d886
chris-townsend <chris-townsend@live.co.uk>
2023-03-07 13:31:05 +0000

    Add comments to serializers.py of project folder & test

--- where_next_drf_api/serializers.py
+++ where_next_drf_api/serializers.py
@@ -3,6 +3,9 @@ from dj_rest_auth.serializers import UserDetailsSerializer
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
+    """
+    Serializer for the CurrentUser which retrieves profile_id & profile_image
+    """
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
 
