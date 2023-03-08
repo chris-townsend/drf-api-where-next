ce10b9c39aa1a795f93359c4e6be4f7c5c37440e
chris-townsend <chris-townsend@live.co.uk>
2023-03-08 15:07:20 +0000

    Add IsAdminUser permission_class to contact views.py

--- contact/views.py
+++ contact/views.py
@@ -2,15 +2,16 @@ from rest_framework import generics, permissions
 from .models import ContactForm
 from .serializers import ContactSerializer
 from where_next_drf_api.permissons import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAdminUser
 
 
 class ContactList(generics.ListCreateAPIView):
     """
     List messages or create a message if the user is logged-in
     """
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = ContactForm.objects.all()
     serializer_class = ContactSerializer
-    permission_classes = [IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -21,6 +22,6 @@ class ContactDetail(generics.RetrieveUpdateDestroyAPIView):
     Retrieve a message and delete it by id if the user is
     logged-in as admin
     """
+    permission_classes = [IsOwnerOrReadOnly, IsAdminUser]
     queryset = ContactForm.objects.all()
     serializer_class = ContactSerializer
-    permission_classes = [permissions.IsAdminUser]
