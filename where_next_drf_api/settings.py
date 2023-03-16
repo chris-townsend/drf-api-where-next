3b7f894ce7203fbc126847016096f227c9bfa215
chris-townsend <chris-townsend@live.co.uk>
2023-03-16 14:59:12 +0000

    Update user_details_serializer in settings.py

--- where_next_drf_api/settings.py
+++ where_next_drf_api/settings.py
@@ -57,7 +57,7 @@ JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'}
+    'USER_DETAILS_SERIALIZER': 'where_next_drf_api.serializers.CurrentUserSerializer'}
 
 
 # Quick-start development settings - unsuitable for production
