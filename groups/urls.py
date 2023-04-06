d840bb7677f502b45a9a1bc4e72ba75e3068c3b5
chris-townsend <chris-townsend@live.co.uk>
2023-04-06 23:08:56 +0000

    Add Join/LeaveGroupView to groups views.py & update urls.py

--- groups/urls.py
+++ groups/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from groups import views
+from groups.views import JoinGroupView, LeaveGroupView
 
 urlpatterns = [
     path('groups/', views.GroupList.as_view()),
     path('groups/<int:pk>/', views.GroupDetail.as_view()),
+    path('groups/<int:pk>/join/', JoinGroupView.as_view()),
+    path('groups/<int:pk>/leave/', LeaveGroupView.as_view()),
 ]
--- groups/views.py
+++ groups/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 from where_next_drf_api.permissons import IsOwnerOrReadOnly
 from .models import Group
 from .serializers import GroupSerializer
@@ -25,3 +26,59 @@ class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Group.objects.all().order_by('-created_date')
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = GroupSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+
+class JoinGroupView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = GroupSerializer
+
+    def get(self, request, pk=None):
+        group = self.get_object()
+        user = request.user
+        if user in group.members.all():
+            return Response(
+                {"detail": "You are already a member of this group."},
+                status=status.HTTP_400_BAD_REQUEST)
+        group.members.add(user)
+        group.save()
+        serializer = GroupSerializer(group)
+        return Response(serializer.data)
+
+    def post(self, request, pk=None):
+        return self.get(request, pk)
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = generics.get_object_or_404(queryset, pk=self.kwargs['pk'])
+        return obj
+
+    def get_queryset(self):
+        return Group.objects.all()
+
+
+class LeaveGroupView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = GroupSerializer
+
+    def post(self, request, pk=None):
+        group = self.get_object()
+        user = request.user
+        if user not in group.members.all():
+            return Response(
+                {"detail": "You are not a member of this group."},
+                status=status.HTTP_400_BAD_REQUEST)
+        group.members.remove(user)
+        group.save()
+        serializer = GroupSerializer(group)
+        return Response(serializer.data)
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = generics.get_object_or_404(queryset, pk=self.kwargs['pk'])
+        return obj
+
+    def get_queryset(self):
+        return Group.objects.all()
